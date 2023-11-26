86a1e5a0713a5643cd30c0e724c15d846772123f
mohamedalysayed <muhammmedaly@gmail.com>
2023-11-26 22:25:38 +0100

    Adding the functionality of renaming files to the selected fuel. Creating a button for adding more paramaters in physicalProperties. Adding the main directories for an OF test case!

--- splash.py
+++ splash.py
@@ -16,7 +16,7 @@ class ReplacePropertiesPopup:
         self.mixture_params = mixture_params
         self.old_values_thermo_type = old_values_thermo_type
         self.old_values_mixture = old_values_mixture
-        self.new_values_thermo_type = {}
+        self.new_values_mixture = {}  
 
         self.popup = tk.Toplevel(parent.root)
         self.popup.title("Update Physical Properties")
@@ -46,12 +46,19 @@ class ReplacePropertiesPopup:
             entry.insert(0, str(old_values_mixture.get(param, "")))  # Pre-fill with old values if available
             entry.config(font=("TkDefaultFont", 9, "bold"), foreground="blue")
             entry.grid(row=row_counter, column=2, pady=2)
-            self.new_values_thermo_type[param] = entry
+            self.new_values_mixture[param] = entry
             row_counter += 1
+            row_counter_plus = row_counter + 1 
 
         # Create an "Update" button that calls the replace_values method for the mixture block
-        ttk.Button(self.popup, text="Update", command=self.replace_mixture_values).grid(row=row_counter, column=2, pady=10, padx=10)
+        style = ttk.Style()
+        style.configure("TButton", padding=10, relief="flat", background="#3EAAAF", foreground="black")
+        updateButton = ttk.Button(self.popup, text="Update", command=self.replace_mixture_values).grid(row=row_counter, column=2, pady=10, padx=10)
+        addButton = ttk.Button(self.popup, text="Add parameter", command=self.add_missing_parameters).grid(row=row_counter_plus, column=2, pady=10, padx=10)
+        #updateButton.pack()
+        #addButton.pack()
 
+        
     def replace_mixture_values(self):
         file_content = self.parent.selected_file_content
 
@@ -68,7 +75,7 @@ class ReplacePropertiesPopup:
         body_content = file_content[foamfile_end_position:]
 
         # Replace old values with new ones in the body content for the mixture block
-        for param, entry in self.new_values_thermo_type.items():
+        for param, entry in self.new_values_mixture.items():
             value = entry.get()
             if value != "":
                 old_pattern = f'{param}\\s*([^;]+)'
@@ -77,12 +84,11 @@ class ReplacePropertiesPopup:
 
         # Combine the header, FoamFile, and updated body content
         file_content = f'{self.parent.header}{foamfile_content}{body_content}'
-        # file_content = f'{foamfile_content}{body_content}'
         
         # Show a confirmation popup
         confirmation = tk.messagebox.askyesno("Confirmation", "Are you sure you want to update the file?")
         if confirmation:
-            # Write the updated content to the file/
+            # Write the updated content to the file
             with open(self.parent.selected_file_path, 'w') as file:
                 file.write(file_content)
 
@@ -92,19 +98,41 @@ class ReplacePropertiesPopup:
         else:
             tk.messagebox.showinfo("Update Canceled", "No changes were made.")
             
+    def add_missing_parameters(self):
+        added_values = {}
+        for prop in self.mixture_params:
+            value = simpledialog.askstring("Add Missing Parameter", f"Enter value for {prop}:")
+            if value is not None:
+                added_values[prop] = value
+
+        if added_values:
+            file_content = self.selected_file_content
+            for prop, value in added_values.items():
+                file_content += f'\n{prop} {value};'
+
+            with open(self.selected_file_path, 'a') as file:
+                file.write(file_content)
+
+            self.status_label.config(text="Parameters added successfully", foreground="green")
+            
             
 class TerminalApp:
     def __init__(self, root):
         self.root = root
         self.root.config(background="white") # black
         self.root.title("Splash some colors here!")
+        
+        # Initialize the available fuels to choose from
+        self.fuels = ["Methanol", "Ammonia", "Dodecane"]
 
         # Create a button to open a directory dialog
-        self.browse_button = ttk.Button(self.root, text="Physical Properties", command=self.browse_directory)
+        style = ttk.Style()
+        style.configure("TButton", padding=10, relief="flat", background="#3EAAAF", foreground="black")
+        self.browse_button = ttk.Button(self.root, text="Update Physical Properties", command=self.browse_directory)
         self.browse_button.grid(row=1, column=2, pady=5, padx=10, sticky="ew")
 
         # Create a label for the "Fuel Selector" dropdown
-        self.fuel_selector_label = ttk.Label(self.root, text="Fuel of choice:", font=("TkDefaultFont", 12, "bold"), foreground="green")
+        self.fuel_selector_label = ttk.Label(self.root, text="Fuel Options", font=("TkDefaultFont", 10, "bold")) # , foreground="green")
         self.fuel_selector_label.grid(row=1, column=0, pady=5, padx=10, sticky="w")
 
         # Define the fuel options
@@ -123,7 +151,6 @@ class TerminalApp:
         self.status_label.grid(row=2, column=0, pady=5, padx=10, sticky="w")
 
         # ... (other initialization code)
-
         self.selected_file_path = None
         self.selected_file_content = None
         self.header = """/*--------------------------------*- C++ -*----------------------------------*\\
@@ -134,28 +161,38 @@ class TerminalApp:
      \\\\/     M anipulation  |
 \\*---------------------------------------------------------------------------*/\n"""
         self.thermo_type_params = ["type", "mixture", "transport", "thermo", "equationOfState", "specie", "energy"]
-        #self.thermo_type_params.config(foreground="blue")
         self.mixture_params = ["molWeight", "rho0", "p0", "B", "gamma", "Cv", "Cp", "Hf", "mu", "Pr"]
 
     def browse_directory(self):
         selected_file = filedialog.askopenfilename()
+
         if selected_file and os.path.basename(selected_file).startswith("physicalProperties"):
             self.selected_file_path = selected_file
             self.status_label.config(text=f"Selected file: {selected_file}", foreground="blue")
 
+            # Update the current fuel status label
+            current_fuel = self.detect_current_fuel()
+            if current_fuel:
+                self.status_label.config(text=f"Current fuel: {current_fuel}", foreground="blue")
+
+                # Check if the detected current fuel matches any of the available fuels
+                # If it does, set it as the selected fuel in the dropdown
+                for fuel in self.fuels:
+                    if fuel.lower() == current_fuel.lower():
+                        self.selected_fuel.set(fuel)
+                        break
+
             with open(selected_file, 'r') as file:
                 file_content = file.read()
                 self.selected_file_content = file_content
-
-            old_values_mixture = {param: match.group(1) for param in self.mixture_params
-                                  for match in re.finditer(f'{param}\s+(\S+)(;|;//.*)', file_content)}
-
-            old_values_thermo_type = {param: match.group(1) for param in self.thermo_type_params
-                                       for match in re.finditer(f'{param}\s+(\S+)(;|;//.*)', file_content)}
-
-            self.open_replace_properties_popup(old_values_mixture, old_values_thermo_type)
+                old_values_mixture = {param: match.group(1) for param in self.mixture_params
+                                      for match in re.finditer(f'{param}\s+(\S+)(;|;//.*)', file_content)}
+                old_values_thermo_type = {param: match.group(1) for param in self.thermo_type_params
+                                           for match in re.finditer(f'{param}\s+(\S+)(;|;//.*)', file_content)}
+                self.open_replace_properties_popup(old_values_mixture, old_values_thermo_type)
         else:
-            tk.messagebox.showerror("Error", "Selected file is not a physicalProperties file! Please select a valid file first.")
+            tk.messagebox.showerror("Error", "Selected file is not a physicalProperties file! Please look for the constant dir in your OF case!")
+            
 
     def open_replace_properties_popup(self, old_values_mixture, old_values_thermo_type):
         selected_file = self.selected_file_path
@@ -165,32 +202,54 @@ class TerminalApp:
 
         else:
             tk.messagebox.showerror("Error", "Please select a valid file first.")
-
-    def add_missing_parameters(self):
-        added_values = {}
-        for prop in self.mixture_params:
-            value = simpledialog.askstring("Add Missing Parameter", f"Enter value for {prop}:")
-            if value is not None:
-                added_values[prop] = value
-
-        if added_values:
-            file_content = self.selected_file_content
-            for prop, value in added_values.items():
-                file_content += f'\n{prop} {value};'
-
-            with open(self.selected_file_path, 'a') as file:
-                file.write(file_content)
-
-            self.status_label.config(text="Parameters added successfully", foreground="green")
-
+            
+    def detect_current_fuel(self):
+        # Assuming the path is in the format '.../OpenFOAM-<version>/cases/<case_name>/constant/physicalProperties'
+        parts = os.path.abspath(self.selected_file_path).split('.')   #split(os.sep)
+        #partsi = os.path.abspath(self.selected_file_path).split('constant')   #split(os.sep)
+        try:
+            #index = parts.index('constant') 
+            current_fuel = parts[1] # 0 + 1
+            #print(current_fuel)
+            return current_fuel
+        except ValueError:
+            return None
+                  
     def on_fuel_selected(self, event):
-        selected_fuel = self.selected_fuel.get()
-        if selected_fuel == "Methanol":
-            print("Selected Methanol. Execute command for Methanol.")
-        elif selected_fuel == "Ammonia":
-            print("Selected Ammonia. Execute command for Ammonia.")
-        elif selected_fuel == "Dodecane":
-            print("Selected Dodecane. Execute command for Dodecane.")
+        selected_fuel = self.selected_fuel.get().lower()
+        if selected_fuel:
+            current_fuel = self.detect_current_fuel()
+            if current_fuel:
+                self.status_label.config(text=f"Current fuel: {current_fuel}", foreground="blue")
+                self.replace_fuel(selected_fuel, current_fuel)
+
+    def replace_fuel(self, selected_fuel, current_fuel):
+        # Assuming the 'constant' directory is inside the case directory
+        case_directory = os.path.dirname(os.path.dirname(self.selected_file_path))
+            
+        
+        # Use find and exec to run sed on all files under the 'constant' directory
+        #sed_command = f"sed -i 's/{current_fuel}/{selected_fuel}/g' {self.selected_file_path}" # maybe change to one higher level up in case we need to change all files
+        sed_command = f"find {case_directory}/constant -type f -exec sed -i 's/{current_fuel}/{selected_fuel}/g' {{}} +"
+        
+        # Execute the sed command
+        subprocess.run(sed_command, shell=True)
+        
+        # --------------------- Renaming files inside constant/ after the selected fuel ----------------------->
+        # Rename the file associated with the current fuel
+        current_file_path = os.path.join(case_directory, 'constant', f'physicalProperties.{current_fuel.lower()}')
+        new_file_path = os.path.join(case_directory, 'constant', f'physicalProperties.{selected_fuel.lower()}')
+        try:
+            os.rename(current_file_path, new_file_path)
+            tk.messagebox.showinfo("Fuel option", f"Fuel has been updated to {selected_fuel}!")
+        except FileNotFoundError:
+            # Handle the case where the file does not exist
+            pass
+        # -----------------------------------------------------------------------------------------------------<
+
+        # Update the status label
+        self.status_label.config(text=f"Fuel replaced. Selected fuel: {selected_fuel}", foreground="green")
+       
             
 if __name__ == "__main__":
     root = tk.Tk()
