7ef5574befb457fdb0a68742cd25c2f73de2605b
mohamedalysayed <muhammmedaly@gmail.com>
2024-05-09 10:17:01 +0200

    Small fix to make things easier!

--- Resources/Build_Splash/createExec.sh
+++ Resources/Build_Splash/createExec.sh
@@ -6,4 +6,4 @@ rm -rf splash build dist splash.spec
 #pyinstaller --onefile --add-data "Resources:Resources" --add-data "Meshing:Meshing" --name splash SplashFOAM.py
 pyinstaller --noconfirm --onefile --add-data "Resources:Resources" --add-data "Meshing:Meshing" --name splash Source/SplashFOAM.py
 
-cp dist/splash . 
+cp dist/splash Source/  
